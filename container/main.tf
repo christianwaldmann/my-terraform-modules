2b5011f1bf96ba7ef1893a7528942a793de58087
christianwaldmann <christianwaldmann>
2024-04-02 17:35:51 +0200

    add provider

--- container/main.tf
+++ container/main.tf
@@ -1,4 +1,12 @@
 
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
 
 resource "docker_image" "whoami" {
   name = "traefik/whoami:latest"
